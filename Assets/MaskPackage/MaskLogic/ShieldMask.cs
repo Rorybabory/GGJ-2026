ef570704d29d1371865b75abc93c40821353a4d3
Edgar Gunther <89051456+DogRoss@users.noreply.github.com>
2026-02-01 03:03:47 -0800

    shield blocks bulet, stops enemy, bumps player
    u herd mi

--- Assets/MaskPackage/MaskLogic/ShieldMask.cs
+++ Assets/MaskPackage/MaskLogic/ShieldMask.cs
@@ -1,16 +1,61 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class ShieldMask : MonoBehaviour
+public class ShieldMask : Mask
 {
+    public Transform shield;
+    public float shieldSpeed = 10f;
+    public float bounce = 50f;
+
+    private Collider shieldCollider;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        shieldCollider = shield.GetComponent<Collider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        print("enter trigger");
+
+        if (other.gameObject.layer == LayerMask.NameToLayer("Bullet"))
+        {
+            GameObject.Destroy(other.gameObject);
+            return;
+        }
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            //shield launches player
+            other.gameObject.GetComponent<Rigidbody>().AddForce((shield.forward + Vector3.up) * bounce);  
+        }
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            //shield stuns agent temporarily.
+            //other.gameObject.GetComponent<NavMeshAgent>().isStopped = true;
+            if(!active)
+                StartCoroutine(StopAgent(other.gameObject.GetComponent<NavMeshAgent>()));
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        print("exit trigger");
+
+        if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            //other.gameObject.GetComponent<NavMeshAgent>().isStopped = false;
+        }
+    }
+    
+    bool active = false;
+    private IEnumerator StopAgent(NavMeshAgent agent)
+    {
+        active = true;
+        agent.isStopped = true;
+        yield return new WaitForSeconds(1.5f);
+        active = false;
+        agent.isStopped = false;
     }
 }
