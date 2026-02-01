224879d20ca4fe423ceb21bdaebef8d9dfe36864
Rorybabory <lambertrory123@gmail.com>
2026-02-01 05:10:16 -0800

    added new wave text

--- Assets/Script_Norman/WaveSpawner.cs
+++ Assets/Script_Norman/WaveSpawner.cs
@@ -6,15 +6,20 @@ public class WaveSpawner : MonoBehaviour
 {
     [SerializeField] GameObject[] prefabs;
     private Transform[] childTransforms;
+    private GameObject waveText;
+    private float waveTextTimer = 0.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        waveText = transform.Find("WaveText").gameObject;
         childTransforms = GetComponentsInChildren<Transform>();
         SpawnWave();
     }
 
     void SpawnWave(int numEnemies = 5)
     {
+        waveTextTimer = 0.0f;
+        waveText.SetActive(true);
         for (int i = 0; i < numEnemies; i++)
         {
             int childIndex = Random.Range(0,childTransforms.Length);
@@ -26,6 +31,14 @@ public class WaveSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (waveText.activeSelf)
+        {
+            waveTextTimer += Time.deltaTime;
+            if (waveTextTimer > 2.0)
+            {
+                waveText.SetActive(false);
+            }
+        }
         if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)
         {
             SpawnWave();
